d920433c8530464c0f2be240fd444a76cf192253
meko568 <mohammedelbardan82@gmail.com>
2026-04-25 23:31:21 +0300

    fixiing the styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n'
 import './index.css'
+import './App.css'
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext'
 
